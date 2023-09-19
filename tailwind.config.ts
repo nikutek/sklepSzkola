7d471082587c06cf5fffe03b2add1738f140a7f7
nikutek <31999595+nikutek@users.noreply.github.com>
2023-09-19 15:26:25 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,10 @@ const { nextui } = require("@nextui-org/react");
 module.exports = {
   content: [
     // ...
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
