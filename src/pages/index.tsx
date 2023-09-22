5ecd8b928fc358831da9a6c255ebd30ed8fc6df3
nikutek <n.golawski@wp.pl>
2023-09-22 16:01:53 +0200

    shadcn dzialaj

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "components/ui/button";
+import { Progress } from "components/ui/progress";
 
 export default function App() {
   return (
-    <>
+    <div className=" flex h-screen w-full flex-col items-center justify-center gap-12">
       <Button>123</Button>
-    </>
+      <Progress value={33} className=" w-1/3" />
+    </div>
   );
 }
