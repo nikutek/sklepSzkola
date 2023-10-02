41efc6489aae158cd57b2c9a5980e5642ba5b3b0
TomosZZZ <okninski.t2004@gmail.com>
2023-10-02 18:30:58 +0200

    Add route protect to profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,3 +1,6 @@
+import { getServerSession } from "next-auth";
+import type { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "~/server/auth";
 import React from "react";
 
 const Profile = () => {
@@ -5,3 +8,23 @@ const Profile = () => {
 };
 
 export default Profile;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const req = context.req as NextApiRequest;
+  const res = context.res as NextApiResponse;
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
