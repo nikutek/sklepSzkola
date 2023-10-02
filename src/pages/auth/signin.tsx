e52b6cc5afecf9413746799ee1dc9f71c475dfb6
TomosZZZ <okninski.t2004@gmail.com>
2023-10-02 18:48:21 +0200

    Add route protecting to signin and verify

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,6 +1,25 @@
 import * as React from "react";
 import Login from "~/components/login";
-
+import { getServerSession } from "next-auth";
+import type { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "~/server/auth";
 export default function signin() {
   return <Login />;
 }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const req = context.req as NextApiRequest;
+  const res = context.res as NextApiResponse;
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/profile",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
--- src/pages/auth/verify.tsx
+++ src/pages/auth/verify.tsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { getServerSession } from "next-auth";
+import type { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "~/server/auth";
 const Verify = () => {
   return <div>verify</div>;
 };
 
 export default Verify;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const req = context.req as NextApiRequest;
+  const res = context.res as NextApiResponse;
+  const session = await getServerSession(req, res, authOptions);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/profile",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
