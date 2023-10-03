f34ea18a37e0875a5ae559469e08b773b0057042
TomosZZZ <okninski.t2004@gmail.com>
2023-10-03 09:40:23 +0200

    Add some logic to verify.tsx

--- src/pages/auth/verify.tsx
+++ src/pages/auth/verify.tsx
@@ -2,8 +2,65 @@ import React from "react";
 import { getServerSession } from "next-auth";
 import type { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
 import { authOptions } from "~/server/auth";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { divider } from "@nextui-org/react";
+
+interface responseObject {
+  message: string;
+}
+
 const Verify = () => {
-  return <div>verify</div>;
+  const [isVerified, setIsVerified] = useState(false);
+  const [verWasSent, setVerWasSent] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const router = useRouter();
+
+  const { email, token } = router.query;
+
+  useEffect(() => {
+    const verifyHandler = async () => {
+      setVerWasSent(true);
+      setIsLoading(true);
+      const response = await fetch("/api/verify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, token }),
+      });
+      if (response.ok) {
+        setIsVerified(true);
+      }
+      const res = (await response.json()) as responseObject;
+      setMessage(res.message);
+      setIsLoading(false);
+    };
+    verifyHandler().catch(console.error);
+  }, [email, token]);
+  return (
+    <div>
+      <div>
+        <h1>Verify your account</h1>
+        {isVerified && (
+          <div>
+            <p>Zweryfikowano</p>
+          </div>
+        )}
+        {verWasSent && !isVerified && !isLoading && (
+          <div>
+            <p>{`${message}`}</p>
+          </div>
+        )}
+        {verWasSent && !isVerified && !isLoading && (
+          <div>
+            <p>Trwa weryfikacja</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Verify;
