60c3882afc2883e833b3dbae20628201af3e3a8b
TomosZZZ <okninski.t2004@gmail.com>
2023-10-07 02:06:34 +0200

    Add responsive header layout. For mobile its on side

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,51 +1,74 @@
 import { Button } from "components/ui/button";
 import Link from "next/link";
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
   const { status } = useSession();
   const logoutHandler = async () => {
     await signOut();
   };
   return (
-    <header className="flex h-[10vh] w-full flex-row  items-center justify-between bg-black px-20 py-3 text-white sm:text-xl">
-      <h1 className="text-5xl">Shop</h1>
-      <div className="flex w-1/2">
-        <nav className="flex w-3/4  flex-row items-center justify-center">
-          <ul className="flex  flex-row items-center justify-center">
-            {status === "authenticated" && (
-              <li className="mx-6 border-b-0 hover:border-b-2">
-                <Link className="pointer " href={"/profile"}>
-                  Profile
-                </Link>
-              </li>
-            )}
-            <li className="mx-6 border-b-0 hover:border-b-2">
-              <Link href={"#"}>Menu 2 </Link>
-            </li>
-            <li className="mx-6 border-b-0 hover:border-b-2">
-              <Link href={"#"}>Menu 3</Link>
-            </li>
-          </ul>
-        </nav>
-        {status === "authenticated" && (
-          <Button
-            onClick={logoutHandler}
-            className="sm:p-6"
-            variant="secondary"
-          >
-            Wyloguj się
-          </Button>
-        )}
-        {status === "unauthenticated" && (
-          <Link className="rounded-lg p-3 hover:border-2" href={"/auth/signin"}>
-            Zaloguj się
-          </Link>
-        )}
+    <Fragment>
+      <div
+        onClick={() => {
+          setMenuVisible((prev) => !prev);
+        }}
+        className={`fixed ${
+          menuVisible ? "left-[260px] " : "left-[10px] "
+        } top-[10px] w-[20px] transition-all`}
+      >
+        X
       </div>
-    </header>
+      <header
+        className={` ${
+          menuVisible ? "left-0" : "-left-[250px]"
+        } fixed  top-0 z-10 h-[100vh] w-[250px] rounded-r-lg bg-black px-2 py-3 text-white transition-all  md:sticky md:left-0 md:flex md:h-[10vh]  md:w-full md:flex-row md:items-center md:justify-between md:rounded-none md:px-10 md:text-xl`}
+      >
+        <h1 className=" mb-10 text-center text-4xl md:mb-0 md:text-5xl">
+          Shop
+        </h1>
+        <div className="flex h-[85vh] flex-col justify-between md:h-auto md:w-3/4 md:flex-row md:items-center">
+          <nav className="flex md:w-3/4 md:flex-row md:items-center md:justify-center">
+            <ul className="flex w-full flex-col md:flex-row md:items-center md:justify-center">
+              {status === "authenticated" && (
+                <li className=" my-4 rounded-lg border-b-4 bg-slate-100 p-2  text-center text-xl text-black transition-all hover:border-4 hover:bg-black hover:text-white md:mx-6 md:rounded-none md:border-0 md:bg-black md:text-white md:hover:border-x-0 md:hover:border-b-2 md:hover:border-t-0">
+                  <Link className="pointer " href={"/profile"}>
+                    Profile
+                  </Link>
+                </li>
+              )}
+              <li className="my-4 rounded-lg border-b-4 bg-slate-100 p-2  text-center text-xl text-black transition-all hover:border-4 hover:bg-black hover:text-white md:mx-6 md:rounded-none md:border-0 md:bg-black md:text-white md:hover:border-x-0 md:hover:border-b-2 md:hover:border-t-0">
+                <Link href={"#"}>Menu 2 </Link>
+              </li>
+              <li className=" my-4 rounded-lg border-b-4 bg-slate-100 p-2  text-center text-xl text-black transition-all hover:border-4 hover:bg-black hover:text-white md:mx-6 md:rounded-none md:border-0 md:bg-black md:text-white md:hover:border-x-0 md:hover:border-b-2 md:hover:border-t-0">
+                <Link href={"#"}>Menu 3</Link>
+              </li>
+            </ul>
+          </nav>
+          {status === "authenticated" && (
+            <Button
+              onClick={logoutHandler}
+              className="md:p-6"
+              variant="secondary"
+            >
+              Wyloguj się
+            </Button>
+          )}
+          {status === "unauthenticated" && (
+            <Button
+              onClick={logoutHandler}
+              className="md:p-6"
+              variant="secondary"
+            >
+              <Link href={"/auth/signin"}>Zaloguj się</Link>
+            </Button>
+          )}
+        </div>
+      </header>
+    </Fragment>
   );
 };
 
