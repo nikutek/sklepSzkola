a0b8c4e483fc6a21b2e20ba36a6601e18c8adfc4
TomosZZZ <okninski.t2004@gmail.com>
2023-10-23 15:26:45 +0200

    conflict fix

--- src/pages/api/categories/index.tsx
+++ src/pages/api/categories/index.tsx
@@ -3,55 +3,54 @@ import { type NextApiResponse, type NextApiRequest } from "next";
 import { db } from "~/server/db";
 
 interface categoryType {
-    category_id: number;
-    name: string;
+  category_id: number;
+  name: string;
 }
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
-  ) {
-    if (req.method === "GET") { 
-        const categories = await db.category.findMany();
-        res.status(200).json(categories);
-        return;
-    }
-    if (req.method ==="POST") {
-        const {name} = req.body as categoryType;
-        const category = await db.category.create({
-          data:{
-            name
-          }
-        })
-        res.status(200).json(category)
-        return ;
-    }
-
-    if (req.method ==="PATCH") {
-        const {category_id, name} = req.body as categoryType;
-        const category = await db.category.update({
-          where:{
-            category_id
-          },
-          data: {
-            name
-          }
-        })  
-
-        res.status(200).json(category)
-        return ;
-    }
-
-    if (req.method ==="DELETE") {
-      const {category_id} = req.body as categoryType;
-      const category = await db.category.delete({
-        where:{
-          category_id
-        },
-      })  
-
-      res.status(200).json(category)
-      return ;
-
-    }
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "GET") {
+    const categories = await db.category.findMany();
+    res.status(200).json(categories);
+    return;
   }
+  if (req.method === "POST") {
+    const { name } = req.body as categoryType;
+    const category = await db.category.create({
+      data: {
+        name,
+      },
+    });
+    res.status(200).json(category);
+    return;
+  }
+
+  if (req.method === "PATCH") {
+    const { category_id, name } = req.body as categoryType;
+    const category = await db.category.update({
+      where: {
+        category_id,
+      },
+      data: {
+        name,
+      },
+    });
+
+    res.status(200).json(category);
+    return;
+  }
+
+  if (req.method === "DELETE") {
+    const { category_id } = req.body as categoryType;
+    const category = await db.category.delete({
+      where: {
+        category_id,
+      },
+    });
+
+    res.status(200).json(category);
+    return;
+  }
+}
