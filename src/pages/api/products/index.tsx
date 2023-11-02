588e8d5be1f8fb08d8f147917fd659e964fb640b
Kotejichizu <n.golawski@wp.pl>
2023-11-02 11:51:04 +0100

    product endpoint fix - mainImage

--- src/pages/api/products/index.tsx
+++ src/pages/api/products/index.tsx
@@ -9,61 +9,72 @@ export interface productType {
   quantity: number;
   description: string;
   isDigital: boolean;
+  mainImage: string;
 }
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse,
-  ) {
-    if (req.method === "GET") { 
-        const products = await db.product.findMany();
-        res.status(200).json(products);
-        return;
-    }
-    if (req.method ==="POST") {
-        const {name, price, quantity, description, isDigital} = req.body as productType;
-        const user = await db.product.create({
-          data:{
-            name,
-            price,
-            quantity,
-            description,
-            isDigital
-          }
-        })
-        res.status(200).json(user)
-        return ;
-    }
-
-    if (req.method ==="PATCH") {
-        const {product_id, name, price, quantity, description, isDigital} = req.body as productType;
-        const user = await db.product.update({
-          where:{
-            product_id
-          },
-          data: {
-            name,
-            price,
-            quantity,
-            description,
-            isDigital
-          }
-        })  
-
-        res.status(200).json(user)
-        return;
-    }
-
-    if (req.method ==="DELETE") {
-      const {product_id} = req.body as productType;
-      const user = await db.product.delete({
-        where:{
-          product_id
-        },
-      })  
-
-      res.status(200).json(user)
-      return ;
-
-    }
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "GET") {
+    const products = await db.product.findMany();
+    res.status(200).json(products);
+    return;
   }
+  if (req.method === "POST") {
+    const { name, price, quantity, description, isDigital, mainImage } =
+      req.body as productType;
+    const product = await db.product.create({
+      data: {
+        name,
+        price,
+        quantity,
+        description,
+        isDigital,
+        mainImage,
+      },
+    });
+    res.status(200).json(product);
+    return;
+  }
+
+  if (req.method === "PATCH") {
+    const {
+      product_id,
+      name,
+      price,
+      quantity,
+      description,
+      isDigital,
+      mainImage,
+    } = req.body as productType;
+    const product = await db.product.update({
+      where: {
+        product_id,
+      },
+      data: {
+        name,
+        price,
+        quantity,
+        description,
+        isDigital,
+        mainImage,
+      },
+    });
+
+    res.status(200).json(product);
+    return;
+  }
+
+  if (req.method === "DELETE") {
+    const { product_id } = req.body as productType;
+    const user = await db.product.delete({
+      where: {
+        product_id,
+      },
+    });
+
+    res.status(200).json(user);
+    return;
+  }
+}
