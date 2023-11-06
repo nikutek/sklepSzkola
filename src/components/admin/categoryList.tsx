483ec6acaa1a9074e02b575a5d018ff4552aab29
TomosZZZ <okninski.t2004@gmail.com>
2023-11-06 16:00:14 +0100

    Add category removing

--- src/components/admin/categoryList.tsx
+++ src/components/admin/categoryList.tsx
@@ -3,8 +3,19 @@ import React, { useEffect, useState } from "react";
 import type { categoryType } from "~/pages/api/categories";
 import { Button } from "components/ui/button";
 import Link from "next/link";
+import { useToast } from "components/ui/use-toast";
 
-const CategoriesListItem = (props: { key: number; name: string }) => {
+const CategoriesListItem = (props: {
+  key: number;
+  name: string;
+  id: number;
+  onDelete: (category: categoryType) => void;
+}) => {
+  const category = {
+    category_id: props.id,
+    name: props.name,
+  };
+
   return (
     <li className="border-grey my-3 flex w-full flex-wrap  items-center justify-between border-b-2 py-2 text-sm md:flex-nowrap md:p-2 md:text-lg">
       <div className="flex w-full  items-center justify-around md:w-[70%] ">
@@ -16,7 +27,10 @@ const CategoriesListItem = (props: { key: number; name: string }) => {
         <Button className=" bg-blue-400 p-4 text-sm hover:bg-blue-500 md:text-lg">
           Edytuj
         </Button>
-        <Button className=" bg-red-700 p-4 text-sm hover:bg-red-800 md:text-lg">
+        <Button
+          onClick={props.onDelete.bind(null, category)}
+          className=" bg-red-700 p-4 text-sm hover:bg-red-800 md:text-lg"
+        >
           Usuń
         </Button>
       </div>
@@ -27,19 +41,46 @@ const CategoriesList = () => {
   const [categories, setCategories] = useState<categoryType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { toast } = useToast();
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch("/api/categories");
+      const data = (await response.json()) as categoryType[];
+      setCategories(data);
+      setIsLoading(false);
+    } catch (err) {}
+  };
   useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await fetch("/api/categories");
-        const data = (await response.json()) as categoryType[];
-        setCategories(data);
-        setIsLoading(false);
-      } catch (err) {}
-    };
     fetchCategories().catch((err) => {
       console.log(err);
     });
   }, []);
+
+  const deleteCategoryHandler = async (category: categoryType) => {
+    console.log("es");
+    const response = await fetch("/api/categories", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(category),
+    });
+    if (!response.ok) {
+      toast({
+        variant: "destructive",
+        title: "Błąd",
+        description: "Coś poszło nie tak",
+      });
+      return;
+    }
+
+    await fetchCategories();
+    toast({
+      title: "Sukces",
+      description: "Pomyślnie usunięto",
+    });
+  };
 
   return (
     <Card className="h-[80vh] max-h-[80vh] w-full overflow-hidden md:w-[70%] ">
@@ -56,6 +97,8 @@ const CategoriesList = () => {
           <ul className="flex flex-col ">
             {categories.map((category: categoryType) => (
               <CategoriesListItem
+                onDelete={deleteCategoryHandler}
+                id={category.category_id}
                 key={category.category_id}
                 name={category.name}
               />
