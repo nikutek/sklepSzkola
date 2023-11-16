2f80b094f6299f9c0d0ee5ed36086925b9d74112
Dawid <85806168+spooky-ddos@users.noreply.github.com>
2023-11-16 22:10:31 +0100

    Create index.tsx

---
+++ src/pages/api/user/index.tsx
@@ -0,0 +1,64 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { db } from "~/server/db";
+
+interface userType {
+  id: string;
+  name: string | null;
+  address: string | null;
+  postal: string | null;
+  post: string | null;
+  email: string | null;
+  password: string;
+  emailVerified: boolean;
+  verificationToken: string;
+  image: string | null;
+  isWorker: boolean;
+  isAdmin: boolean;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "PATCH") {
+    const {
+      id,
+      name,
+      address,
+      postal,
+      post,
+      email,
+      password,
+      emailVerified,
+      image,
+      isWorker,
+      isAdmin,
+    } = req.body as userType;
+    const user = await db.user.update({
+      where: { id },
+      data: {
+        id,
+        name,
+        address,
+        postal,
+        post,
+        email,
+        password,
+        emailVerified,
+        image,
+        isWorker,
+        isAdmin,
+      },
+    });
+
+    res.status(200).json(user);
+    return;
+  }
+
+  if (req.method == "DELETE") {
+    const { id } = req.body as userType;
+    const user = await db.user.delete({ where: { id } });
+    res.status(200).json(user);
+    return;
+  }
+}
