1c8cf9fc11f896347ce8fc7003d2d207527d4bb7
TomosZZZ <okninski.t2004@gmail.com>
2023-11-27 19:57:43 +0100

    Add shop page with products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Button } from "components/ui/button";
 import { Progress } from "components/ui/progress";
 import { IKImage, IKVideo, IKContext, IKUpload } from "imagekitio-react";
+import Products from "~/components/shop/Products";
 
 export default function App() {
   const authenticator = async () => {
@@ -33,44 +34,8 @@ export default function App() {
   };
 
   return (
-    <div className=" flex h-screen w-full flex-col items-center justify-center gap-12">
-      <Button>123</Button>
-      <Progress value={33} className=" w-1/3" />
-      <input
-        type="file"
-        id="image_uploads"
-        name="image_uploads"
-        accept=".jpg, .jpeg, .png, .jfif"
-        multiple
-        // onChange={handleFileChange}
-      />
-      {/* //In order to use the SDK, you need to provide it with a few configuration
-      parameters. //The configuration parameters can be applied directly to the
-      IKImage component or using //an IKContext component. */}
-      <IKContext
-        publicKey="public_/+LytKfMQpryszlk5TiCCBsHWzE="
-        urlEndpoint="https://ik.imagekit.io/szkolaaaa"
-        transformationPosition="path"
-        authenticator={authenticator}
-      >
-        {/* // Image component */}
-        <IKImage
-          path="/default-image.jpg"
-          transformation={[
-            {
-              height: "300",
-              width: "400",
-            },
-          ]}
-        />
-        {/* // Image upload */}
-        <IKUpload
-          fileName="my-upload"
-          onError={(err) => {
-            console.log(err);
-          }}
-        />
-      </IKContext>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-12 bg-[#111]">
+      <Products />
     </div>
   );
 }
