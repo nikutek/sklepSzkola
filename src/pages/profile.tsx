e27480000a963ddb6b561ab16bac970e2d24cfe7
Kotejichizu <n.golawski@wp.pl>
2023-11-27 23:02:27 +0100

    UserProfilGgsp

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,9 +2,20 @@ import { getServerSession } from "next-auth";
 import type { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
 import { authOptions } from "~/server/auth";
 import React from "react";
+import { type productType } from "~/components/admin/productList";
+import { type orderType } from "./api/orders";
+import { useEffect, useState } from "react";
+import Product from "./admin/add/product";
 
-const Profile = () => {
-  return <div>profile</div>;
+const Profile = (props) => {
+  const [userData, setUserData] = useState<productType>(props.userData);
+  console.log(userData);
+
+  return (
+    <div className="bg-black-600 w-full w-full">
+      <div>profile {userData.name}</div>
+    </div>
+  );
 };
 
 export default Profile;
@@ -22,9 +33,36 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
+  interface userDataType {
+    id: string;
+    name: string;
+    address: string;
+    postal: string;
+    post: string;
+    email: string;
+    password: string;
+    emailVerified: true;
+    verificationToken: string;
+    image: string | null;
+    isWorker: boolean;
+    isAdmin: boolean;
+    orders: orderType[];
+    favoriteProducts: productType[];
+  }
+
+  const userEndpointResponse = await fetch(
+    `http://localhost:3000/api/user/${session.user.email}`,
+    {
+      method: "GET",
+    },
+  );
+
+  const userData = (await userEndpointResponse.json()) as userDataType;
+
   return {
     props: {
       session,
+      userData,
     },
   };
 };
