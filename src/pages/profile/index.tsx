07b47da1bf47094043b3c6cc2f5975526fa2d35b
Kotejichizu <n.golawski@wp.pl>
2023-12-07 19:48:49 +0100

    profile page production fix

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -198,7 +198,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 
   const userEndpointResponse = await fetch(
-    `http://localhost:3000/api/user/${session.user.email}`,
+    `https://sklepszkola-production.up.railway.app/${session.user.email}`,
     {
       method: "GET",
     },
