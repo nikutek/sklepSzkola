d9b09b8e2c457bf2c5205166189504e3fd1014c4
Kotejichizu <n.golawski@wp.pl>
2023-12-07 15:17:30 +0100

    product page fix

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -19,7 +19,7 @@ export default ProductPage;
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const id = query.id as string;
-  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+  const response = await fetch(`/api/products/${id}`);
   if (!response.ok) {
     return { props: {} };
   }
